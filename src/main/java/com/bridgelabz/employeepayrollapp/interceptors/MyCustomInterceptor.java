983535fcc2f49311d741e44762111ce250e52caf
Aditya Verma <aaadarths2357@gmail.com>
2020-12-14 17:08:29 +0530

    Added: @Sl4j annotation to implement logging using lombok library

--- src/main/java/com/bridgelabz/employeepayrollapp/interceptors/MyCustomInterceptor.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/interceptors/MyCustomInterceptor.java
@@ -3,16 +3,15 @@ package com.bridgelabz.employeepayrollapp.interceptors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 public class MyCustomInterceptor implements HandlerInterceptor{
-	
-	private static final Logger log=LogManager.getLogger(MyCustomInterceptor.class); 
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
