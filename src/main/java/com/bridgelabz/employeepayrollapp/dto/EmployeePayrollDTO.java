74e992125a26e4d80551828c408b7bb3c03c8df1
Aditya Verma <aaadarths2357@gmail.com>
2020-12-14 19:11:35 +0530

    Added: code to validate fields in the employeepayrollDto to avoid further processing of the spring application to other layers

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -2,11 +2,19 @@ package com.bridgelabz.employeepayrollapp.dto;
 
 import java.util.Date;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 
-public @Data class EmployeePayrollDTO {
-
+public @Data class EmployeePayrollDTO { 
+	
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}([ ][A-Z]{1}[a-zA-Z\\s]{2,})?$",
+			message = "Employee name is Invalid")
 	public String name;
+	@Min(value = 30000, message = "Salary should be more than Rs. 30,000")
+	@Max(value = 80000, message = "Salary should be less than Rs. 80,000")
 	public long salary;
 	public String profilePicture;
 	public String[] departments;
